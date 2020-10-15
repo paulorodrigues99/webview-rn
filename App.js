6cda8c6d9a7ba055332536690a3cf2feb34090e7
Paulo Rodrigues <paulo.rodrigues@PEINSMTNB2267.insole.local>
2020-10-14 23:07:53 -0300

    :tada:

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+
+class App extends Component {
+  render() {
+    return (
+      <WebView
+        style={styles.container}
+        source={{ uri: 'https://insole.com.br' }}
+        style={{ marginTop: 30 }}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
+export default App;
